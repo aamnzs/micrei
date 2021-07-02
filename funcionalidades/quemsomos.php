9d19dbfdd856694af712748446c8205c7a6a1ecd
Arildo Alves de Menezes <arildoalves@gmail.com>
2021-07-01 22:17:34 -0300

    0000 - Versão_001 do Prpjeto MICREI

---
+++ funcionalidades/quemsomos.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Quem somos...</title>
+    </head>
+    <body>
+	<?php
+	include '../inc/esqueleto.php';
+	?>
+<!-- Quem somos" -->
+<div class="container-fluid">
+	<div class="card border-0">
+		<div class="card-header bg-faded text-primary">
+			<p><h3 class="card-title" align="center">Este será seu local para aquisição e socialização de conhecimentos</h3></p>
+		</div>
+		<span class="border border-primary">
+			<div class="card-body bg-faded">
+				<p class="card-text text-justify">O site foi idealizado para fornecer informações de qualidade visando potencializar as chances de sucesso das pessoas que decidem formalizar ou expandir empreendimentos.</p>
+				<div class="text-center">
+					<P>
+						<img src="../img/Logo_Micrei_1.jpeg" class="img-fluid"  width="100" alt="logomarca">
+					</P>
+				</div>
+				<p class="card-text text-justify">A logomarca possibilita vislumbrar uma letra M, uma porta entreaberta e um funil dentro de um quadrado vermelho.</p>
+				<p class="card-text text-justify">A letra M é a inicial da sigla <strong>MICREI</strong> que serviu de base para o acrônimo <font color="#FF0000"><b><i>M</i></b></font><i>elhores</i> <font color="#FF0000"><b><i>I</i></b></font><i>nformações</i> <font color="#FF0000"><b><i>C</i></b></font><i>onquistam</i> <font color="#FF0000"><b><i>R</i></b></font><i>esultados</i>  <font color="#FF0000"><b><i>E</i></b></font><i>xcepcionalmente</i>  <font color="#FF0000"><b><i>I</i></b></font><i>ncríveis.</i></p>
+				<p class="card-text text-justify">A porta entreaberta representa uma passagem estreita que levará empreendedores a ingressarem em novos ambientes.</p>
+				<p class="card-text text-justify">O funil se refere ao filtro pelo qual passarão para alcançarem o sucesso desejado.</p>
+				<p class="card-text text-justify">O quadrado vermelho retrata os diversos mercados onde precisarão demonstrar competências e habilidades superiores para se manterem e prosperaram na vida empresarial.</p>
+				<p class="card-text text-justify">A cor branca representa uma incógnita, pois impede a visão e o conhecimento prévio de oportunidades e obstáculos existentes além da porta.</p>
+				<!-- Detalhes da Missão, Visão e Valores - Início... -->
+				<div class="container-fluid">
+					<ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
+						<li class="nav-item">
+							<a class="nav-link active" id="missao-tab" data-toggle="tab" href="#missao" role="tab" aria-controls="missao" aria-selected="true">Missão</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" id="visao-tab" data-toggle="tab" href="#visao" role="tab" aria-controls="visao" aria-selected="false">Visão</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" id="valores-tab" data-toggle="tab" href="#valores" role="tab" aria-controls="valores" aria-selected="false">Valores</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" id="objetivo-tab" data-toggle="tab" href="#objetivo" role="tab" aria-controls="objetivo" aria-selected="false">Objetivos</a>
+						</li>
+					</ul>
+					<div class="tab-content" id="myTabContent">
+						<div class="tab-pane fade show active" id="missao" role="tabpanel" aria-labelledby="missao-tab">
+							<p class="card-text text-justify"><b><i>Ampliar os conhecimentos das pessoas interessadas em iniciar ou aprimorar negócios.</i></b></p>
+						</div>
+						<div class="tab-pane fade" id="visao" role="tabpanel" aria-labelledby="visao-tab">
+							<p class="card-text text-justify"><b><i>Ser a melhor fonte de informações para empreendedores.</i></b></p>
+						</div>
+						<div class="tab-pane fade" id="valores" role="tabpanel" aria-labelledby="valores-tab">
+							<p class="card-text text-justify"><b><i>Maturidade; Inovação; Cooperação; Respeito; Empatia e Integridade.</i></b></p>
+						</div>
+						<div class="tab-pane fade" id="objetivo" role="tabpanel" aria-labelledby="objetivo-tab">
+							<p class="card-text text-justify"><b><i>Aprimorar a gestão empresarial e contribuir para a longevidade de negócios.</i></b></p>
+						</div>
+					</div>
+				</div>
+				<!-- Detalhes da Missão, Visão e Valores - Final... -->
+			</div>
+			</span>
+		</div>
+	</div>
+</div>
+	<br>
+	<div class="container-fluid">
+		<div class="card border-0">
+		    <div class="card-header bg-faded text-primary">
+			<p><h3 class="card-title" align="center">Oportunidades</h3></p>
+			</div>
+		    <span class="border border-primary">
+			<div class="card-body bg-faded">
+			    <p class="card-text text-justify">Se você percebeu afinidade com nossa <i>Missão, Visão, Valores e Objetivos</i>, poderemos discutir possibilidades de parcerias em diversas áreas de atuações empresariais.</p>
+			    <p class="card-text text-justify">O site <b><i>www.micrei.com</i></b> é a primeira parte de um programa composto por vários projetos direcionados para o desenvolvimento pessoal e apresentação de sugestões de soluções para as principais dificuldades e necessidades dos empreendedores.</p>
+			    <p class="card-text text-justify">Após nossa mentoria através do site, faremos acompanhamentos dos novos empreendedores e precisaremos montar equipe de especialistas multidisciplinares para atuações nos mais variados negócios e oportunidades.</p>
+			    <p class="card-text text-justify">Você poderá participar ajudando-nos com suas experiências e habilidades, sugerindo novas funcionalidades e divulgando o site para seus amigos, familiares e demais conhecidos.</p>
+			    <p class="card-text text-justify">Entre em contato conosco para obter informações adicionais.<font color="#FF0000"><b><i>(Incluir formulário para contato)</i></b></font></p>
+			</div>
+		    </span>
+		</div>
+	</div><br><br><br>
+    </body>
+</html>
