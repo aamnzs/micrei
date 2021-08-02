d2c556f7d577d71da7f3798ff79f3f700c9dc861
Arildo Alves de Menezes <arildoalves@gmail.com>
2021-08-02 19:55:18 -0300

    V_2021_07_Projeto MICREI

---
+++ _exemplos/2021_07_27_esqueleto.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Favicon -->
+    <link rel="sortcut icon" href="favicon_micrei.jpeg" type="image/favicon.jpeg"/>
+    <title>Esqueleto</title>
+  </head>
+<body>
+<header>
+<div class="container-fluid">
+  <nav class="navbar navbar-expand-lg d-flex justify-content-between navbar border-0 fixed-top navbar-light alert-warning">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Contato</a>
+      <a href="web/index.php"><img src="../dados/img/Logo_Micrei_1.jpeg" class="img-fluid" width="50" alt="logomarca"/></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarScroll">
+        <ul class="navbar-nav me-auto my-2 my-lg-0 " style="--bs-scroll-height: 100px;">
+          <li class="nav-item">
+            <a class="nav-link " aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Mentoria</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Ferramentas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Mapa do site</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pesquisa</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Digite os termos da pesquisa" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Pesquisa</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</div>
+
+
+
+
+
+
+
+
+  <!--
+  <div class="container-fluid">
+    <nav class="nav d-flex justify-content-between navbar border-0 fixed-top navbar-light alert-warning">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">MICREI</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+          <ul class="navbar-nav">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">MENU</a>
+            <li class="nav-item dropdown">
+              
+              <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                <li><a class="dropdown-item" href="#">Home</a></li>
+                <li><a class="dropdown-item" href="#">Mentoria</a></li>
+                <li><a class="dropdown-item" href="#">Ferramentas</a></li>
+                <li><a class="dropdown-item" href="#">Quem somos</a></li>
+                <li><a class="dropdown-item" href="#">Mapa do site</a></li>
+                <li><a class="dropdown-item" href="#">Contato</a></li>
+              </ul>
+            </li>
+            
+          </ul>
+          <a href="../funcionalidades/quemsomos.php"><h1 class="text-xl-center text-danger">M I C R E I</h1></a>
+            <a href="https://www.imagensanimadas.com/cat-chaves-273.htm" target="_blank"><img src="https://www.imagensanimadas.com/data/media/273/chave-imagem-animada-0032.gif" border="0" alt="login" width="50" /></a>
+        </div>
+      </div>
+    </nav>
+  </div>
+  
+  <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav class="nav d-flex justify-content-between navbar border-0 fixed-top navbar-light alert-warning">
+        <div class="container-fluid">
+          <div class="container" id="nav-container">
+            <nav class="navbar navbar-light bg-light">
+              <div class="container-fluid">
+                
+                <a class="btn  dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">MENU</a>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                  <li><a class="dropdown-item" href="#">Home</a></li>
+                  <li><a class="dropdown-item" href="#">Mentoria</a></li>
+                  <li><a class="dropdown-item" href="#">Ferramentas</a></li>
+                  <li><a class="dropdown-item" href="#">Quem somos</a></li>
+                  <li><a class="dropdown-item" href="#">Mapa do site</a></li>
+                  <li><a class="dropdown-item" href="#">Contato</a></li>
+                </ul>
+                
+              </div>
+              <a href="../funcionalidades/quemsomos.php"><h1 class="text-xl-center text-danger">M I C R E I</h1></a>
+              <a href="https://www.imagensanimadas.com/cat-chaves-273.htm" target="_blank"><img src="https://www.imagensanimadas.com/data/media/273/chave-imagem-animada-0032.gif" border="0" alt="login" width="50" /></a>
+            </nav>
+          </div>
+          
+          
+        </div>
+      </nav>
+    </div>
+    <div class="container-fluid" style="margin-top:65px"></div>
+    -->
+  </header>
+        <!-- <h1 class="text-xl-center">Funcionalidade em desenvolvimento ....</h1> -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ _exemplos/2021_07_31_esqueleto.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <!-- Meta tags Obrigatórias -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <!-- Favicon -->
+        <link rel="sortcut icon" href="../img/favicon_micrei.jpeg" type="image/favicon.jpeg"/>
+        <title>MICREI</title>
+    </head>
+    <body>
+<!-- Menu principal de TODAS as páginas do site -->
+<header>
+    <div class="container-fluid fixed-top">
+        <nav class="navbar navbar-light bg-dark">
+            <div class="container-fluid">
+                <div class="dropdown">
+                    <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Menu</button>
+                    <ul class="dropdown-menu bg-info" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">Mentoria</a></li>
+                        <li><a class="dropdown-item" href="#">Ferramentas</a></li>
+                        <li><a class="dropdown-item" href="../dados/funcionalidades/quemsomos.php" target="_blank">Quem somos</a></li>
+                        <li><a class="dropdown-item" href="#">Mapa do site</a></li>
+                        <li><a class="dropdown-item" href="#">Contato</a></li>
+                    </ul>
+                </div>
+                <a href="http://localhost/micrei/web/"><h1 class="text-xl-center text-light">M I C R E I</h1></a>
+                <a href="https://www.imagensanimadas.com/cat-chaves-273.htm"><img src="https://www.imagensanimadas.com/data/media/273/chave-imagem-animada-0032.gif" border="0" alt="login" width="50" /></a>
+            </div>
+        </nav>
+    </div>
+     <div class="container-fluid" style="margin-top:65px"></div>
+</header>
+            <!-- A partir daqui entrará os conteúdos do site...         
+            <div class="container-fluid">
+            <div class="container-fluid bg-info">Linha de teste</div>
+            </div>
+            -->
+<!-- Informações de configurações gerais... -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
